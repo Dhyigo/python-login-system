697ee6bd04aed9433a29bd2c9af871588c71f2cd
Dhyego <aldhiego666@gmail.com>
2022-03-22 18:01:23 -0300

    login_python

--- sistema_login.py
+++ sistema_login.py
@@ -16,8 +16,12 @@ while True:
 
     if opcao == '1':
         novo_nome = str(input('\nCRIAR USUÁRIO: ')).strip()
-        nova_senha = str(input('CRIAR SENHA: ')).strip()
+
         if novo_nome not in lista_nome:
+            nova_senha = str(input('CRIAR SENHA: ')).strip()
+            while len(nova_senha) < 3:
+                print('\033[33mSENHA DEVE TER MÍNIMO DE 3 CARACTERES\033[m')
+                nova_senha = str(input('CRIAR SENHA: ')).strip()
 
             lista_nome.append(novo_nome)
             lista_senha.append(nova_senha)
@@ -45,23 +49,28 @@ while True:
         login_nome = str(input('\nUSUÁRIO: ')).strip()
 
         if login_nome in lista_nome:
+
+            for i in lista_nome:
+
+                if login_nome in lista_nome[lugar]:
+                    break
+
+                lugar += 1
+
             for c in range(4, 0, -1):
                 login_senha = str(input('SENHA: ')).strip()
                 print('SENHA INCORRETA VOCÊ TEM {} TENTATIVAS'.format(c - 1))
-            if login_senha in lista_senha:
-                for i in lista_nome:
-
-                    if login_nome in lista_nome[lugar]:
-                        break
-
-                    lugar += 1
-
-                if login_nome in lista_nome[lugar] and login_senha in lista_senha[lugar]:
-                    print('\n\033[32mLOGIN EFETUADO COM SUCESSO!\033[m\nSEJA BEM VINDO {}\n'
-                          .format(login_nome.capitalize()))
+                if login_senha == lista_senha[lugar]:
                     break
 
+
+            if login_nome == lista_nome[lugar] and login_senha == lista_senha[lugar]:
+                print('\n\033[32mLOGIN EFETUADO COM SUCESSO!\033[m\nSEJA BEM VINDO {}\n'
+                      .format(login_nome.capitalize()))
+                break
+
             else:
+
                 print('\n\033[31mSENHA INVÁLIDA!!\nTENTE NOVAMENTE.\033[m\n')
 
         else:
